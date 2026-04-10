81429f409c2e39eb2471f81fbffdb87f5f103c22
ueeet <lehasellaki@gmail.com>
2026-04-10 19:23:36 +0300

    auto: update

---
+++ backend/prisma.config.ts
@@ -0,0 +1,20 @@
+import "dotenv/config";
+import path from "node:path";
+import { defineConfig } from "prisma/config";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+export default defineConfig({
+  schema: path.join("prisma", "schema.prisma"),
+  migrations: {
+    path: path.join("prisma", "migrations"),
+  },
+  experimental: {
+    adapter: true,
+  },
+  adapter: async () => {
+    return new PrismaPg({
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
+    });
+  },
+});
