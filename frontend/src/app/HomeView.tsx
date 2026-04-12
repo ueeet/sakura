185da206c55a3129fadc0dc839b07310ec2f36cb
ueeet <lehasellaki@gmail.com>
2026-04-12 19:27:25 +0300

    auto: update

--- frontend/src/app/HomeView.tsx
+++ frontend/src/app/HomeView.tsx
@@ -489,6 +489,8 @@ export function HomeView({
             </div>
           </div>
         </section>
+        {/* ===== REVIEWS ===== */}
+        <ReviewsSection reviews={reviews} />
       </main>
       <Footer />
     </>
